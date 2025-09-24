e5b6cdb33bacf35065e0ade8c534dd28aaa4a356
Manju-M <manjumatagar542@gmail.com>
2025-09-24 14:49:31 +0530

    Update App.java

--- src/main/java/KOTAK/App.java
+++ src/main/java/KOTAK/App.java
@@ -5,7 +5,7 @@ package KOTAK;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello manju!");
       
     }
 }
